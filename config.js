18522e8f4eadea8c7b22ed284ecbd3f1c01d900a
thetimfulton <77407668+thetimfulton@users.noreply.github.com>
2026-02-20 14:44:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     NREL_API_KEY: 'PLVvOusL7wjyAy2ZMcd0MFroNaL8sGI3hguxkHkm',
     GA_MEASUREMENT_ID: 'G-WP97VS7YGP',
-    CLARITY_ID: 'vkh3yv7jkj'
+    CLARITY_ID: 'vkh3yv7jkj',
     FORMSPREE_ID: 'xdalljoz' 
 };
 
