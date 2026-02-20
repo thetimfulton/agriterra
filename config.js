223d62a0a90827c19dc422557744f88400e76689
thetimfulton <77407668+thetimfulton@users.noreply.github.com>
2026-02-20 14:16:52 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    NREL_API_KEY: 'PLVvOusL7wjyAy2ZMcd0MFroNaL8sGI3hguxkHkm',
+    GA_MEASUREMENT_ID: 'G-WP97VS7YGP',
+    CLARITY_ID: 'vkh3yv7jkj'
+};
+
+// This makes the config accessible to your other files
+window.AGRI_CONFIG = CONFIG;
